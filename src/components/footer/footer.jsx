677fdeb29cebf6d8bef8bf44332911eb91795c9f
abd alsayad <134537357+abdsyd@users.noreply.github.com>
2025-01-17 21:04:16 +0300

    improved the footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,52 +1,55 @@
-import React from "react";
+
 import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 import './footer.css'
 
 export default function Footer() {
     return (
-        <div className="app">
-            <div className="content">
-                {/* Your main content goes here */}
+        <footer className="text-center mt-auto">
+        <div className="container p-2">
+            <div>
+                <a href="#" className="mx-3">
+                    <FaFacebookF />
+                </a>
+                <a href="#" className="mx-3">
+                    <FaTwitter />
+                </a>
+                <a href="#" className="mx-3">
+                    <FaInstagram />
+                </a>
+                <a href="#" className="mx-3">
+                    <FaLinkedin />
+                </a>
             </div>
-
-            <footer className=" text-white text-center py-4 mt-auto">
-                <div className="container">
-                    <p className="mb-2">&copy; 2025 Suleiman. All Rights Reserved.</p>
-                    <ul className="list-inline">
-                        <li className="list-inline-item">
-                            <a href="#" className="text-white text-decoration-none">
-                                Privacy Policy
-                            </a>
-                        </li>
-                        <li className="list-inline-item">|</li>
-                        <li className="list-inline-item">
-                            <a href="#" className="text-white text-decoration-none">
-                                Terms of Service
-                            </a>
-                        </li>
-                        <li className="list-inline-item">|</li>
-                        <li className="list-inline-item">
-                            <a href="#" className="text-white text-decoration-none">
-                                Contact Us
-                            </a>
-                        </li>
-                    </ul>
-                    <div className="mt-3">
-                        <a href="#" className="text-white mx-2">
-                            <FaFacebookF />
+            <div>&copy; 2025 Suleiman. All Rights Reserved.</div>
+            <div>
+                <ul className="list-inline">
+                    <li className="list-inline-item">
+                        <a href="#" className="text-decoration-none">
+                            Privacy Policy
                         </a>
-                        <a href="#" className="text-white mx-2">
-                            <FaTwitter />
+                    </li>
+                    <li className="list-inline-item">|</li>
+                    <li className="list-inline-item">
+                        <a href="#" className="text-decoration-none">
+                            Terms of Service
                         </a>
-                        <a href="#" className="text-white mx-2">
-                            <FaInstagram />
+                    </li>
+                    <li className="list-inline-item">|</li>
+                    <li className="list-inline-item">
+                        <a href="#" className="text-decoration-none">
+                            Contact Us
                         </a>
-                        <a href="#" className="text-white mx-2">
-                            <FaLinkedin />
-                        </a>
-                    </div>
-                </div>
-            </footer>
+                    </li>
+                </ul>
+            </div>
         </div>
+    </footer>
+    
     );
 }
+/*
+
+
+
+
+*/
